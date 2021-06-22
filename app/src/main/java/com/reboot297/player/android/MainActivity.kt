6f5c7d478de7343d6a00fe376a842168b56658e2
Reboot297 <reboot297@gmail.com>
2021-06-22 11:39:29 +0300

    initial commit

---
+++ app/src/main/java/com/reboot297/player/android/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.reboot297.player.android
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
