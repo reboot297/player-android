c5ebbd3fca0df9fc872b8bb578a1d5f35b61ce13
Reboot297 <reboot297@gmail.com>
2021-06-22 12:26:19 +0300

    updated dependencie versions

---
+++ buildSrc/src/main/java/Deps.java
@@ -0,0 +1,18 @@
+
+
+public class Deps {
+    public static final String androidPlugin = "com.android.tools.build:gradle:4.2.1";
+
+    public static final String kotlinVersion = "1.5.10";
+    public static final String kotlinPlugin = "org.jetbrains.kotlin:kotlin-gradle-plugin:" + kotlinVersion;
+    public static final String kotlinRuntime = "org.jetbrains.kotlin:kotlin-stdlib:" + kotlinVersion;
+    public static final String ktx = "androidx.core:core-ktx:1.5.0";
+
+    public static final String appCompat = "androidx.appcompat:appcompat:1.3.0";
+    public static final String material = "com.google.android.material:material:1.3.0";
+    public static final String constraintLayout = "androidx.constraintlayout:constraintlayout:2.0.4";
+
+    public static final String junit = "junit:junit:4.13.2";
+    public static final String androidJunit = "androidx.test.ext:junit:1.1.2";
+    public static final String espresso = "androidx.test.espresso:espresso-core:3.3.0";
+}
